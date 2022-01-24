56f2f3035d0cae6ac683b89689e47761366087da
fmixson <flmixson@cerritos.edu>
2022-01-24 15:19:23 -0800

    012422 commit

--- main.py
+++ main.py
@@ -5,28 +5,29 @@ import easygui
 from pathlib import Path
 
 pd.set_option('display.max_columns', None)
-currentUndecidedFileDF = pd.read_excel('UndecidedStudents.xlsx', index_col=0)
+currentUndecidedFileDF = pd.read_excel('UndecidedStudents.xlsx')
+currentUndecidedFileDF.to_excel('UndecidedStudents.xlsx')
 
 print(currentUndecidedFileDF)
 newUndecidedFile = easygui.fileopenbox()
-newUndecidedFileDF = pd.read_excel(newUndecidedFile, 'Undecided Units')
+newUndecidedFileDF = pd.read_excel(newUndecidedFile, 'Undecided')
 print(newUndecidedFileDF)
 
 remainingUndedecided = []
 for i in range(len(currentUndecidedFileDF) - 1):
     for j in range(len(newUndecidedFileDF) - 1):
-        if currentUndecidedFileDF.loc[i, 'Employee ID'] == newUndecidedFileDF.loc[j, 'Employee ID']:
+        if currentUndecidedFileDF.loc[i, 'Student External ID'] == newUndecidedFileDF.loc[j, 'Student External ID']:
             remainingUndedecided.append(i)
 
 currentUndecided = []
 for i in range(len(newUndecidedFileDF)):
     for j in range(len(currentUndecidedFileDF)):
-        if newUndecidedFileDF.loc[i, 'Employee ID'] == currentUndecidedFileDF.loc[j, 'Employee ID']:
+        if newUndecidedFileDF.loc[i, 'Student External ID'] == currentUndecidedFileDF.loc[j, 'Student External ID']:
             currentUndecided.append(i)
 newUndecidedDF = newUndecidedFileDF.drop(currentUndecided)
-writer2 = pd.ExcelWriter('NewUndecided.xlsx', engine='xlsxwriter')
-newUndecidedDF.to_excel(writer2, sheet_name='NewUndecided')
-writer2.save()
+# writer2 = pd.ExcelWriter('NewUndecided.xlsx', engine='xlsxwriter')
+# newUndecidedDF.to_excel(writer2, sheet_name='NewUndecided')
+# writer2.save()
 
 home = Path.home()
 saveUndecidedFile = Path(home, 'Desktop', 'NewUndecided.xlsx')
